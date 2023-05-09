a62a0b8994faa02d1ba0b6dfe25ef87d3e0b3db6
FelixSnz <felixsanchez0124@gmail.com>
2023-05-09 11:28:30 -0500

    windows service installer class

---
+++ SerialIDTracker/Installer1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace SerialIDTracker
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+            ServiceProcessInstaller processInstaller = new ServiceProcessInstaller();
+
+            processInstaller.Account = ServiceAccount.LocalSystem;
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            serviceInstaller.ServiceName = "IdTrackerService";
+            serviceInstaller.DisplayName = "IdTrackerService";
+            serviceInstaller.Description = "A sample Windows service created in C#.";
+
+            Installers.Add(serviceInstaller);
+            Installers.Add(processInstaller);
+        }
+    }
+}
