f53f911c197c709f77c0f61e4a38167332562f25
FelixSnz <felixsanchez0124@gmail.com>
2023-05-09 13:56:37 -0500

    named pipe client refactored and working

--- SerialIDTracker/Services/NamedPipeClient.cs
+++ SerialIDTracker/Services/NamedPipeClient.cs
@@ -1,36 +1,67 @@
 ﻿using System;
 using System.IO.Pipes;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SerialIDTracker.Services
 {
-    public class NamedPipeClient : IDisposable
+    internal class NamedPipeClient
     {
-        private readonly NamedPipeClientStream client;
+        private NamedPipeClientStream _pipeClient;
+        private const string PipeName = "SerialTrackerService";
+        private const int BufferSize = 4096;
 
-        public NamedPipeClient(string pipeName)
+        public event EventHandler<string> DataReceived;
+
+        public NamedPipeClient()
         {
-            client = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut, PipeOptions.None);
-            client.Connect();
+            Initialize();
         }
 
-        public void Send(string message)
+        private void Initialize()
         {
-            byte[] data = Encoding.UTF8.GetBytes(message);
-            client.Write(data, 0, data.Length);
+            _pipeClient = new NamedPipeClientStream(".", PipeName, PipeDirection.InOut, PipeOptions.Asynchronous);
+            _pipeClient.Connect();
         }
 
-        public string Receive()
+        public void Connect()
         {
-            byte[] buffer = new byte[1024];
-            int bytesRead = client.Read(buffer, 0, buffer.Length);
-            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-            return message;
+             _pipeClient.Connect(5000);
+            //StartListening();
+        }
+
+        private void StartListening()
+        {
+            byte[] buffer = new byte[BufferSize];
+            _pipeClient.BeginRead(buffer, 0, buffer.Length, OnDataReceived, buffer);
+        }
+
+        private void OnDataReceived(IAsyncResult asyncResult)
+        {
+            int bytesRead = _pipeClient.EndRead(asyncResult);
+
+            if (bytesRead > 0)
+            {
+                byte[] buffer = asyncResult.AsyncState as byte[];
+                string receivedData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                DataReceived?.Invoke(this, receivedData);
+
+                _pipeClient.BeginRead(buffer, 0, buffer.Length, OnDataReceived, buffer);
+            }
+        }
+
+        public void SendData(string data)
+        {
+            if (_pipeClient.IsConnected)
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(data);
+                _pipeClient.Write(buffer, 0, buffer.Length);
+            }
         }
 
         public void Dispose()
         {
-            client.Dispose();
+            _pipeClient?.Dispose();
         }
     }
 }
--- SerialIDTracker/Services/Producer.cs
+++ SerialIDTracker/Services/Producer.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+using ILogger = NLog.ILogger;
+
 
 namespace SerialIDTracker.Services
 {
     internal class Producer
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
 
+        static NamedPipeClient namedPClient = new NamedPipeClient();
+        
         public static  void OnDataScanned(object sender, string Data)
         {
-            NamedPipeClient namedPClient = new NamedPipeClient("SerialTrackerService");
-            namedPClient.Send(Data);
+            
+            //Logger.Info(Data);
+            namedPClient.SendData(Data);
         }
     }
 }
